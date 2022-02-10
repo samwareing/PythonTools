cc9b88094040005f23de34ae996e778ebdb8e3b3
samwareing <98537672+samwareing@users.noreply.github.com>
2022-02-10 09:30:56 +0000

    Added some docstrings to convert_dates_to_quarters

--- convert_dates_to_quarters.py
+++ convert_dates_to_quarters.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+from typing import List
 
 
-def convert_to_datetime(date, date_format='%d/%m/%Y'):
+def convert_to_datetime(date, date_format='%d/%m/%Y') -> datetime:
+    '''Converts the string to datetime using the English format.'''
     return datetime.strptime(date, date_format)
 
 
-def convert_to_quarter_text(datet: datetime):
+def convert_to_quarter_text(datet: datetime) -> str:
+    '''Converts the datetime to the correct quarter and year format we needed.'''
     year = datet.year
     month = datet.month
     if month in (1, 2, 3):
@@ -22,6 +25,7 @@ def convert_to_quarter_text(datet: datetime):
 
 
 def main():
+    '''Main script to convert a list of dates into QQ / YY format.'''
     dates = ['04/01/2022', '04/01/2023', '', '04/01/2024']
 
     quarter_year_texts = []
